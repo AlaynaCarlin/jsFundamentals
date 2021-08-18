eba843a9ddbfb05d14e3f1e2baeb2003921b7de9
AlaynaCarlin <alaynacarlin18@gmail.com>
2021-08-18 19:23:55 -0400

    First commit

---
+++ 1-grammerAndTypes/01-comments-and-console.js
@@ -0,0 +1,39 @@
+/* COMMENTS 
+
+1-grammerAndTypes
+    01-comments-and-console.js
+*/
+
+// value of a comment: 
+// communicates to anyone reading our code
+
+//types of comments:
+//1) single-line comment 
+/* multi-line comments
+stuff :) */
+
+//console.log
+//outputs some message to the console
+console.log("this is a console.log message");
+
+console.log('you can separate parts of your message with commas,','just like this!')
+
+console.log(23,34,45,56);
+
+// console.log and debigging
+//we can debug with console.lig in two main ways:
+//1) we can check the status of some javascript 'thing'
+//2) we can check a process we have built with repeated console.log messages
+
+//example 1:
+var sampleName = 'Alayna';
+console.log(sampleName);
+
+//example 2:
+var sampleNum = 1;
+console.log(sampleNum);
+sampleNum++;
+console.log(sampleNum);
+sampleNum++;
+console.log(sampleNum);
+
